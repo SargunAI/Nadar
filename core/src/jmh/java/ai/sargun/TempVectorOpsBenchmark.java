1092f8b221d94eb633fb5a9e3c67040c208ce153
hardikxk <hardikkumarpro0005@gmail.com>
2026-05-28 00:00:18 +0530

    sargun

--- core/src/jmh/java/ai/sargun/TempVectorOpsBenchmark.java
+++ core/src/jmh/java/ai/sargun/TempVectorOpsBenchmark.java
@@ -81,10 +81,6 @@ public class TempVectorOpsBenchmark {
     public float scale_flattened_allocating() {
         try (Arena localArena = Arena.ofConfined()) {
             NDArray out = NDArray.zeros(DType.FLOAT32, localArena, a.shape);
-
-            // Paste your flattened loop logic here
-            // ...
-
             return out.getFloat(0);
         }
     }
